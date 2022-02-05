0b13801685d3bc250aa538344eb0b389459deec3
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-02-05 18:39:41 +0530

    Add files via upload

---
+++ Elephant.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+
+int x,step;
+
+cin>>x;
+
+step = x / 5;
+    if (x % 5 > 0) {
+        step++;
+    }
+    cout << step<<endl;
+
+
+
+    return 0;
+
+
+
+
+
+
+
+}
