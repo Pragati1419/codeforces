79150a2f108b80f5d2a79d86655b4cfac94d83bd
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-02-20 21:38:46 +0530

    Add files via upload

---
+++ minorsum.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        int n;
+        cin>>n;
+
+        vector<int>arr(n);
+
+        for(int i=0;i<n;++i){
+            cin>>arr[i];
+
+        }
+
+        int x=arr.front();
+
+        for(auto &y:arr)
+        x |=y;
+
+        cout<<x<<endl;
+    }
+
+    return 0;
+}
